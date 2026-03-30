cea43f93faa0b0a2e3486d6f1b8f6891eb82f704
rachelGithub-1 <118177691+rachelGithub-1@users.noreply.github.com>
2026-03-30 13:08:55 +0000

    Initial commit

--- src/config/appConfig.js
+++ src/config/appConfig.js
@@ -1,3 +1,22 @@
+// const appConfig = {
+//   environment: process.env.REACT_APP_ENV,
+//   apiUrl: process.env.REACT_APP_API_URL,
+//   appName: process.env.REACT_APP_APP_NAME,
+//   logLevel: process.env.REACT_APP_LOG_LEVEL,
+//   analyticsEnabled: process.env.REACT_APP_ENABLE_ANALYTICS === "true"
+// };
+
+// export default appConfig;
+
+
+
+//We only want to print config when the app is in dev mode.
+//This variable is the condition to do that safely.
+//read the environment variable set by react and compare to the string "development,
+//then store the getValue, either true/false in the the variable 'is'
+ 
+const isDevelopment = process.env.NODE_ENV === "development";
+ 
 const appConfig = {
   environment: process.env.REACT_APP_ENV,
   apiUrl: process.env.REACT_APP_API_URL,
@@ -5,5 +24,11 @@ const appConfig = {
   logLevel: process.env.REACT_APP_LOG_LEVEL,
   analyticsEnabled: process.env.REACT_APP_ENABLE_ANALYTICS === "true"
 };
-
+ 
+if (isDevelopment) {
+  console.groupCollapsed(" appConfig is in development mode");
+  console.log(appConfig);
+  console.groupEnd();
+}
+ 
 export default appConfig;
