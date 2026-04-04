a2d07699bbc70ca4a6778c2f318ce2f28d48a155
rachelGithub-1 <118177691+rachelGithub-1@users.noreply.github.com>
2026-04-04 23:14:29 +0000

    Move dev config viewer into App

--- src/App.js
+++ src/App.js
@@ -1,21 +1,17 @@
 import React from "react";
 import Dashboard from "./pages/Dashboard";
 import EnvironmentBanner from "./components/EnvironmentBanner";
+import ConfigViewer from "./components/ConfigViewer";
+import { isDevelopment } from "./config/appConfig";
 
 function App() {
-
   return (
-
     <div>
-
       <EnvironmentBanner />
-
+      {isDevelopment && <ConfigViewer />}
       <Dashboard />
-
     </div>
-
   );
-
 }
 
 export default App;
--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,12 +1,9 @@
 import React from "react";
-import ConfigViewer from "../components/ConfigViewer";
-import { isDevelopment } from "../config/appConfig";
 
 function Dashboard() {
   return (
     <div style={{ padding: 30 }}>
       <h1>Hello from my local machine</h1>
-      {isDevelopment && <ConfigViewer />}
     </div>
   );
 }
